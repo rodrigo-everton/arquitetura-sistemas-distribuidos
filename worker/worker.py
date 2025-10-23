50f6d602c3f09d81e64f33376b3a8fbb212d257d
cassiap <cassiiagabrielaa@hotmail.com>
2025-10-23 08:45:05 -0300

    atualizado

---
+++ worker/worker.py
@@ -0,0 +1,68 @@
+import socket
+import json
+import threading
+import uuid
+import time
+        
+def conexao(s, worker_id, HOST, PORT):
+    s.connect((HOST,PORT))
+    workerAlive = {"WORKER":"ALIVE",
+                   "WORKER_UUID":worker_id,}
+    msg = json.dumps(workerAlive) + "\n"
+    s.sendall(msg.encode())
+
+if __name__ == '__main__':
+    worker_id = str(uuid.uuid4())
+    current_master = '192.168.15.6'
+    while True:
+        HOST = current_master
+        PORT = 5001
+
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+            conection_thread = threading.Thread(target=conexao, args=(s, worker_id, HOST, PORT,))
+            s.settimeout(30)
+            conection_thread.start()
+            conection_thread.join(timeout=30)
+            redirect = False
+            try:
+                while True:
+                    data = s.recv(1024).decode()
+                    if not data:
+                        break
+                    answer = json.loads(data)
+                    #print(answer)               
+                    if answer.get("TASK") == "REDIRECT":
+                        target = answer.get("MASTER_REDIRECT")
+                        if target:
+                            current_master = target
+                            redirect = True
+                            break
+                    if answer.get("TASK") == "QUERY":
+                        output = {
+                                "WORKER_UUID":worker_id,
+                                "CPF":"11111111111",
+                                "SALDO":"25.000",
+                                "TASK":"QUERY",
+                                "STATUS":"OK"
+                            }
+                        time.sleep(1)
+                        s.sendall((json.dumps(output) + "\n").encode())
+                        print(answer)
+                        print(output)
+                    else:
+                        output = {
+                                "WORKER":"EDUARDO",
+                                "CPF":"11111111111",
+                                "SALDO":0,
+                                "TASK":"QUERY",
+                                "STATUS":"NOK",
+                                "ERROR":"User not found"
+                            }
+                        time.sleep(1)
+                        s.sendall((json.dumps(output) + "\n").encode())
+                        print(answer)
+                        print(output)
+            except Exception:
+                print("Error, reconnecting...")
+                time.sleep(0.5)
+                pass
